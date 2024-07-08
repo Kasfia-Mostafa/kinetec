c1a08d23c7ed66bb84309664f09f6d7fe0c53772
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-07-08 23:26:32 +0600

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
